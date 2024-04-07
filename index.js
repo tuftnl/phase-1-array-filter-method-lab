34b3707a907406dc9eb8040a08ebe08d04fc35a6
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-07 16:11:41 -0400

    some problems with filter definitely check back later

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+function findMatching(arr,str) {
+    return arr.filter((string) => string.toLowerCase() === str.toLowerCase())
+}
+function fuzzyMatch(arr,str) {
+    return arr.filter((string) => 
+    string.toLowerCase().indexOf(str.toLowerCase()) === 0)
+}
+function matchName(arr,str){
+    return arr.filter((record) => record.name === str)
+}
